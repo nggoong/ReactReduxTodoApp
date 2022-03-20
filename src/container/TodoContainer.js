2f35d21e0744515a2e8f70a20eb3b1c138597981
nggoong <nggoong@naver.com>
2022-03-20 16:30:16 +0900

    useSelector, useDispatch를 사용하여 redux를 hooks를 사용하는 로직으로 전환✔

--- src/container/TodoContainer.js
+++ src/container/TodoContainer.js
@@ -1,31 +1,47 @@
-import { connect, useSelector } from "react-redux";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import TodoList from "../components/TodoList";
 import { changeInput, resetInput, toggle, insert, remove } from "../modules/todos";
 
-const TodoContainer = ({ changeInput, resetInput, toggle, insert, remove }) => {
-    const todos = useSelector(state => state.todos.todos);
-    const input = useSelector(state => state.todos.input);
+const TodoContainer = () => {
+    const { input, todos } = useSelector(({ todos }) =>({
+        input: todos.input,
+        todos: todos.todos
+    }))
+    const dispatch = useDispatch();
+
+    const onChagneInput = useCallback((input)=> {
+        dispatch(changeInput(input));
+    }, [dispatch])
+    
+    const onInsert = useCallback((text) => {
+        dispatch(insert(text));
+    }, [dispatch]);
+
+    const onResetInput = useCallback(()=> {
+        dispatch(resetInput());
+    }, [dispatch])
+
+    const onToggle = useCallback((id)=> {
+        dispatch(toggle(id));
+    }, [dispatch])
+
+    const onRemove = useCallback((id)=>{
+        dispatch(remove(id));
+    }, [dispatch])
     
     return(
-        <TodoList todos={todos} input={input} onChangeInput={changeInput}
-        onResetInput={resetInput}
-        onToggle={toggle} onInsert={insert} onRemove={remove}/>
+        <TodoList todos={todos}
+        input={input}
+        onChangeInput={onChagneInput}
+        onResetInput={onResetInput}
+        onToggle={onToggle}
+        onInsert={onInsert}
+        onRemove={onRemove}/>
     )
 }
 
 
 
 
-export default connect(
-    state => ({
-        todos: state.todos.todos,
-        input: state.todos.input,
-    }),
-    {
-        changeInput,
-        resetInput,
-        toggle,
-        insert,
-        remove
-    }   
-)(TodoContainer);
+export default TodoContainer;
